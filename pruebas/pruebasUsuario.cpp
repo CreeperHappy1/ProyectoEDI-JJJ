78b74cc48dde1b94f69e1c5496ff3d23fdf53cb5
rubberTriangle <2006jorgesg@gmail.com>
2025-03-31 10:59:01 +0200

    Pruebas de usuario

--- pruebas/pruebasUsuario.cpp
+++ pruebas/pruebasUsuario.cpp
@@ -1,5 +1,138 @@
 #include "pruebas.h"
+#include <iostream>
+using namespace std;
 
 void pruebaUsuario(){
-    //Me pasó lo mismo (que con pruebasCuentaBancaria.cpp), dolor, las pruebas estaban bien, aunque estas no tenían punteros, sin más confía en que funcionaban :((
+    
+    cout << "INICIO PRUEBAS USUARIO" << endl;
+    
+    Usuario user;
+    
+    //Pruebas apellidosNombre
+    string cad1;
+    
+    user.setApellidosNombre("Sánchez Gil, Jorge");
+    
+    //prueba supervisada
+    
+    cout << user.getApellidosNombre() << endl;
+    
+    //prueba automatica
+    
+    if(user.getApellidosNombre() != "Sánchez Gil, Jorge"){
+        cerr << "Error en la prueba de poner apellido" << endl;
+    }
+    
+    user.getApellidosNombre(cad1);
+    cout << cad1 << endl;
+    
+    //Pruebas telefono
+    
+    string cad2;
+    
+    user.setTelefono("902 45 86 47");
+    
+    //prueba supervisada
+    
+    cout << user.getTelefono() << endl;
+    
+    //prueba automatica
+    
+    if(user.getTelefono() != "902 45 86 47"){
+        cerr << "Error en la prueba de poner telefono" << endl;
+    }
+    
+    user.getTelefono(cad2);
+    cout << cad2 << endl;
+    
+    //Pruebas edad
+    
+    int num;
+    
+    user.setEdad(30);
+    
+    //prueba supervisada
+    
+    cout << user.getEdad() << endl;
+    
+    //prueba automatica
+    
+    if(user.getEdad() != 30){
+        cerr << "Error en la prueba de poner edad" << endl;
+    }
+    
+    user.getEdad(num);
+    cout << num << endl;
+    
+    //Pruebas mostrar
+    
+    user.mostrar();
+    
+    //Pruebas pasarACadena
+    
+    string cad3;
+    
+    cad3 = user.pasarACadena();
+    
+    cout << cad3 << endl;
+    
+    //PRUEBAS CUENTA BANCARIA
+    
+    //Pruebas de constructores
+    
+    //Caso 1 constructor por defecto
+    
+    user.setCuentaBancaria("ES3222", 100);
+    
+    //Pruebas de getters
+    
+    //Caso 1 getNumeroCuenta
+    
+    cout << "El número de cuenta es: ES3222, y sale: " << user.getNumeroCuenta() << endl;
+    
+    //Caso 2 getSaldo
+    
+    cout << "El saldo de la cuenta es: 100, y es: " << user.getSaldo() << endl;
+    
+    //Pruebas de setters
+    
+    //Caso 1 setNumeroCuenta
+    
+    cout << "El número de cuenta es: " << user.getNumeroCuenta() << endl;
+    
+    user.setNumeroCuentaBancaria("HY4555");
+    
+    cout << "El nuevo número de cuenta es, HY4555, y debería ser: " << user.getNumeroCuenta() << endl;
+    
+    //Caso 2 setSaldo
+    
+    cout << "El saldo de la cuenta es: " << user.getSaldo() << endl;
+    
+    user.setSaldo(200);
+    
+    cout << "El saldo de la cuenta es: 200, y sale: " << user.getSaldo() << endl;
+    
+    //Caso 3 ingresar
+    
+    cout << "El saldo de la cuenta es: " << user.getSaldo() << endl;
+    
+    user.ingresar(100);
+    
+    cout << "El saldo de la cuenta es: 300, y sale: " << user.getSaldo() << endl;
+    
+    //Caso 4 retirar
+    
+    cout << "El saldo de la cuenta es: " << user.getSaldo() << endl;
+    
+    user.retirar(100);
+    
+    cout << "El saldo de la cuenta es: 200, y sale: " << user.getSaldo() << endl;
+    
+    //Mostrar
+    
+    cout << " El saldo de la cuenta es: " << user.getSaldo() << " El número de la cuenta es: " << user.getNumeroCuenta() << endl;
+    
+    user.mostrarCuentaBancaria();
+    
+    cout << "FIN PRUEBAS USUARIO" << endl;
 }
