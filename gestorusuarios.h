934893d26119d0a6bb52171b4d36fb05ff6d30e0
CreeperHappy1 <jorgejuan.azpeitia.pozo@gmail.com>
2025-04-28 10:03:10 +0200

    comentario duplicado quitado

--- gestorusuarios.h
+++ gestorusuarios.h
@@ -34,7 +34,7 @@ public:
     GestorUsuarios();
     GestorUsuarios(GestorUsuarios const& other);
     void insertar(const std::string &apellidoNombre, const std::string &telefono, int edad, const std::string numeroCuenta, const float saldo, const std::string DNI, const std::string email);
-    Usuario* buscar(const std::string DNI);//Mariscal ha puesto como header "bool buscarUsuario(string dni, Usuario *&usu);" pero si quiero devolver fallo devuelvo nullptr o una excepción
+    Usuario* buscar(const std::string DNI);
     const int numElementos();
     void mostrar();
     void eliminarUsuario(const std::string DNI);//busca un usuario y lo elimina de la lista (se elimina el objeto [composición])
