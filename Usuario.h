5d371c7c6dc685f237cc3724a64ec9d91ffac707
rubberTriangle <2006jorgesg@gmail.com>
2025-05-29 20:37:35 +0200

    Terminado

--- Usuario.h
+++ Usuario.h
@@ -1,4 +1,3 @@
-//~ = alt gr + 4
 #ifndef USUARIO_H
 #define USUARIO_H
 #include <string>
@@ -13,46 +12,171 @@ private:
     std::string DNI;
     std::string email;
 public:
+    
+    //PRE: No hay
+    //DESC: Inicia los valores de los string apellidoNombre, telefono, DNI y email a "", el int edad a -1 y se inicia la cuenta bancaria
+    //Complejidad: O(1)
     Usuario();
+    
+    //PRE: Parámetros de entrada y salida string apellidoNombre, telefono, DNI, email y el número de cuenta, el int edad, el float saldo y la cuenta bancaria
+    //DESC: Asigna los valores de los parámetros de entrada a este usuario
+    //Complejidad: O(1)
     Usuario(const std::string &apellidoNombre, const std::string &telefono, int edad, const std::string numeroCuenta, const float saldo, const std::string DNI, const std::string email);
+    
+    //PRE: Parámetro de entrada y salida de tipo Usuario llamado original
+    //DESC: Asigna los valores del usuario pasado por parámetro a este
+    //Complejidad: O(1)
     Usuario(const Usuario &original);
+    
+    //PRE: No hay
+    //DESC: Destruye la cuenta bancaria y el usuario
+    //Complejidad: O(1)
     ~Usuario();
     
+    
     //Métodos selectores
+    
+    //PRE: No hay
+    //DESC: Devuelve el nombre del usuario
+    //Complejidad: O(1)
     std::string getNombre() const;
+    
+    //PRE: Parámetro de entrada y salida apellidoNombre
+    //DESC: Devuelve el nombre del usuario mediante el parámetro de entrada y salida
+    //Complejidad: O(1)
     void getNombre(std::string &apellidoNombre);
+    
+    //PRE: No hay
+    //DESC: Devuelve el teléfono del usuario
+    //Complejidad: O(1)
     std::string getTelefono() const;
+    
+    //PRE: Parámetro de entrada y salida telefono
+    //DESC: Devuelve el teléfono del usuario mediante el parámetro de entrada y salida
+    //Complejidad: O(1)
     void getTelefono(std::string &telefono);
+    
+    //PRE: No hay
+    //DESC: Devuelve la edad del usuario
+    //Complejidad: O(1)
     int getEdad() const;
+    
+    //PRE: Parámetro de entrada y salida tipo int de la edad
+    //DESC: Devuelve la edad del usuario mediante el parámetro de entrada y salida
+    //Complejidad: O(1)
     void getEdad(int &edad);
+    
+    //PRE: No hay
+    //DESC: Devuelve el string dni del usuario
+    //Complejidad: O(1)
     std::string getDNI();
+    
+    //PRE: Parámetro de entrada y salida de tipo string DNI
+    //DESC: Devuelve el DNI del usuario mediante el parámetro de entrada y salida
+    //Complejidad: O(1)
     void getDNI(std::string &DNI);
+    
+    //PRE: No hay
+    //DESC: Devuelve el email del usuario
+    //Complejidad: O(1)
     std::string getEmail();
+    
+    //PRE: Parámetro de entrada y salida de tipo string
+    //DESC: Devuelve el email del usuario mediante el parámetro de entrada y salida
+    //Complejidad: O(1)
     void getEmail(std::string &email);
     
+    
     //Métodos modificadores
+    
+    //PRE: Parámetro de entrada de tipo string apellidoNombre
+    //DESC: Cambia el nombre del usuario según el parámetro de entrada
+    //Complejidad: O(1)
     void setNombre(std::string apellidoNombre);
+    
+    //PRE: Parámetro de entrada de tipo string telefono
+    //DESC: Cambia el teléfono del usuario según el parámetro de entrada
+    //Complejidad: O(1)
     void setTelefono(std::string telefono);
+    
+    //PRE: Parámetro de entrada de tipo string edad
+    //DESC: Cambia la edad del usuario según el parámetro de entrada
+    //Complejidad: O(1)
     void setEdad(const int edad);
+    
+    //PRE: Parámetro de entrada de tipo string numeroCuenta y de tipo float saldo
+    //DESC: Cambia el numero de cuenta y el saldo del usuario según los parámetros de entrada
+    //Complejidad: O(1)
     void setCuentaBancaria(const std::string numeroCuenta, const float saldo);
+    
+    //PRE: Parámetro de entrada de tipo string newDNI
+    //DESC: Cambia el DNI del usuario según el parámetro de entrada
+    //Complejidad: O(1)
     void setDNI(const std::string newDNI);
+    
+    //PRE: Parámetro de entrada de tipo string newEmail
+    //DESC: Cambia el email del usuario según el parámetro de entrada
+    //Complejidad: O(1)
     void setEmail(const std::string newEmail);
     
+    
     //otros
+    
+    //PRE: No hay
+    //DESC: Pasa a una cadena de texto todos los parámetros del usuario
+    //Complejidad: O(1)
     std::string pasarACadena();
+    
+    //PRE: No hay
+    //DESC: Muestra los atributos del usuario en pantalla
+    //Complejidad: O(1)
     void mostrar();
     
+    
     //métodos de cuenta bancaria
-        std::string getNumeroCuenta() const;
-        void getNumeroCuenta(std::string &num);
-        float getSaldo() const;
-        void getSaldo(float &saldo);
-        //setters
-        void setNumeroCuenta(const std::string newNumeroCuenta);
-        void setSaldo(float newSaldo);
-        //otros
-        void mostrarCuenta();
-        void ingresar(const float dS);//no voy a hacer un sacarDinero cuando puedes sin más dS < 0
+    
+    //PRE: No hay
+    //DESC: Devuelve el número de cuenta de la cuenta bancaria en forma de string
+    //Complejidad: O(1)
+    std::string getNumeroCuenta() const;
+    
+    //PRE: Parámetro de entrada y salida de tipo string
+    //DESC: Devuelve el número de cuenta mediante el parámetro de entrada y salida
+    //Complejidad: O(1)
+    void getNumeroCuenta(std::string &num);
+    
+    //PRE: No hay
+    //DESC: Devuelve el saldo de la cuenta bancaria en forma de float
+    //Complejidad: O(1)
+    float getSaldo() const;
+    
+    //PRE: Parámetro de entrada y salida de tipo float
+    //DESC: Devuelve el saldo de la cuenta bancaria mediante el parámetro de entrada y salida
+    //Complejidad:
+    void getSaldo(float &saldo);
+    //setters
+    
+    //PRE: Parámetro de entrada de tipo string
+    //DESC: Cambia el número de la cuenta bancaria mediante el parámetro de entrada
+    //Complejidad: O(1)
+    void setNumeroCuenta(const std::string newNumeroCuenta);
+    
+    //PRE: Parámetro de entrada de tipo float
+    //DESC: Cambia el saldo de la cuenta bancaria mediante el parámetro de entrada
+    //Complejidad: O(1)
+    void setSaldo(const float newSaldo);
+    //otros
+    
+    //PRE: No hay
+    //DESC: Muestra los valores de la cuenta bancaria
+    //Complejidad: O(1)
+    void mostrarCuenta();
+    
+    
+    //PRE: Parámetro de entrada de tipo float
+    //DESC: Aumenta el saldo de la cuenta bancaria según el parámetro de entrada
+    //Complejidad: O(1)
+    void ingresar(const float dS);//no voy a hacer un sacarDinero cuando puedes sin más dS < 1
 };
 
 #endif // USUARIO_H
--- sistema.h
+++ sistema.h
@@ -15,35 +15,112 @@ private:
     ListaDPI <Patinete *> *lPatinetes;
     ListaDPI <Estacion *> *lEstaciones;
     
+    //PRE: No hay
+    //DESC: Carga desde el fichero usuarios.csv los datos de los usuarios al gestorUsuarios
+    //Complejidad: O(n²)
     void cargarUsuarios();//Este método debe ser privado y se invocará en los constructores de esta clase. Esta operación debe procesar el fichero de texto usuarios.csv, usando los datos leídos de cada usuario para invocar al método insertar de la clase GestorUsuarios.
+    
+    //PRE: No hay
+    //DESC: Carga desde el fichero patinetes.csv los datos de los patinetes a la lista de Patinetes
+    //Complejidad: O(n²)
     void cargarPatinetes();//Si un valor bool es diferente de Si se asume false//Este método debe ser privado y se invocará en los constructores de esta clase. Esta operación debe procesar el fichero de texto patinetes.csv, usando los datos leídos de cada patinete para invocar al método insertarPatinete de esta clase
+    
+    //PRE: No hay
+    //DESC: Carga desde el fichero estaciones.csv los datos de las estaciones en la lista de Estaciones
+    //Complejidad: O(n²)
     void cargarEstaciones();
     
+    
+    //PRE: No hay
+    //DESC: Carga desde el ficher distribucionPatinetes.csv los datos de en qué estación está cada patinete
+    //Complejidad: O(n²)
     void distribuirPatinetesEnEstaciones();
+    
+    //PRE: Parámetros de entrada y salida string de id origen, DNI y id destino
+    //DESC: Un usuario alquila un patinete, siendo los parámetros de entrada, idEstOrigen la estación de la que alquila el patinete, idEstDestino la estación en la que deja el patinete y DNI la persona que lo alquila
+    //Complejidad: O(1)
     void alquilarDevolverUnPatinete(std::string const& idEstOrigen, std::string const& DNI, std::string const& idEstDestino);
     
 public:
+    
+    //PRE: No hay
+    //DESC: Inicia el nombre del sistema a "" e inicia los módulos de cargarUsuarios, cargarPatinetes y cargarEstaciones para procesar los archivos de texto
+    //Complejidad: O(n²)
     Sistema();//NOTE: no tiene sentido que el constructor por defecto cargue nada, sería mejor que los metodos para cargar tomasen como argumento en path al .csv
+    
+    //PRE: Parámetro de entrada string para el nombre del sistema
+    //DESC: Inicia el nombre del sistema según el parámetro de entrada e inicia los módulos de cargarUsuarios, cargarPatinetes y cargarEstaciones para procesar los archivos de texto
+    //Complejidad: O(n²)
     Sistema(std::string nombreSistema);//NOTE: quizá esto debería ser const&
-
+    
+    //PRE: Parámetro de entrada y salida de otro sistema
+    //DESC: Copia el otro sistema en este
+    //Complejidad: O(n)
     Sistema(Sistema const& other);//por copia
     
+    //PRE: No hay
+    //DESC: Muestra el número de usuarios y los usuarios del sistema
+    //Complejidad: O(n)
     void mostrarUsuarios();//muestra el número de usuarios total del sistema y la información de todos ellos.
+    
+    //PRE: Parámetro de entrada string
+    //DESC: Busca un usuario en la lista de usuarios con el DNI de entrada
+    //Complejidad: O(n)
     std::string buscarUsuario(std::string const DNI);//recibe como parámetro de entrada el DNI de un usuario y, si lo encuentra, devuelve toda la información del usuario (p.ej., para luego poder mostrarlo).
     
+    //PRE: Parámetros de entrada string identificador, marca y modelo, y bool averiado y disponible
+    //DESC: Inserta un patinete al final de la lista de patinetes
+    //Complejidad: O(1)
     void insertarPatinete(const std::string identificador, const std::string marca, const std::string modelo, const bool averiado, const bool disponible);//Añade un nuevo patinete a la lista lPatinetes. Este método recibe como parámetros de entrada los datos de un patinete y los utilizará para crear una instancia dinámica (puntero a un objeto) de la clase Patinete. Una vez creado el puntero al patinete, lo insertará en la lista lPatinetes. El orden de inserción queda a criterio de los desarrolladores.
+    
+    //PRE: No hay
+    //DESC: Muestra los patinetes en la lista de patinetes y dice el número que hay
+    //Complejidad: O(n)
     void mostrarPatinetes();//muestra el número de patinetes total del sistema y la información de todos ellos.
+    
+    //PRE: Parámetro de entrada string identificador
+    //DESC: Busca y devuelve el patinete de la lista de patinetes según el identificador del patrón de entrada
+    //Complejidad: O(n)
     Patinete* buscarPatinete(std::string const identificador);
     
+    //PRE: Parámetros de entrada string identificador y dirección
+    //DESC: Inserta una nueva estación con identificador y dirección del parámetro de entrada
+    //Complejidad: O(1)
     void insertarEstacion(std::string identificador, std::string direccion);
+    
+    //PRE: No hay
+    //DESC: Muestra los parámetros de las estaciones y los patinetes en ellos
+    //Complejidad: O(n²)
     void mostrarEstaciones();
+    
+    //PRE: Parámetro de entrada string identificador
+    //DESC: Busca la estación con el identificador que hay desde el parámetro de entrada
+    //Complejidad: O(n)
     Estacion* buscarEstacion(std::string const identificador);
     
+    //PRE: Parámetros string, identificador de patinete e identificador de estación
+    //DESC: Agrega el patinete con el identificador del parámetro de entrada y lo añade a la estación con el identificador del parámetro de entrada
+    //Complejidad: O(n)
     void agregarPatineteEnEstacion(std::string identificadorP, std::string identificadorE);
+    
+    //PRE: No hay
+    //DESC: Lee el fichero alquilerPatinetes.csv y alquila los patinetes a los respectivos usuarios
+    //Complejidad: O(n)
     void alquilarDevolverPatinetes();//lee automáticamente del fichero "alquilerPatinetes.csv"
+    
+    //PRE: Parámetro de entrada string identificador de estación
+    //DESC: Repara los patinetes de la estación cuyo identificador es el del parámetro de entrada
+    //Complejidad: O(n)
     int repararPatinetesEstacion(std::string const identificadorE);
+    
+    //PRE: No hay
+    //DESC: Busca los patinetes extraviados
+    //Complejidad: O(n²)
     void buscarPatinetesExtraviados();
     
+    //PRE: No hay
+    //DESC: Elimina los elementos de las listas de estaciones y patinetes y también elimina las propias listas
+    //Complejidad: O(n)
     ~Sistema();
 };
 
